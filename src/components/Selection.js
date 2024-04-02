2d2d6ceca0cf5f0a6d712b1bddd915d7e7d1e25d
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-04-02 20:06:51 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,18 @@
 
+import React from 'react';
+import './Selection.css';
+
+const Selection = (props) => {   
+    const { applyColor } = props;
+     function changeColor(e){
+        e.target.style.backgroundColor = applyColor.background;
+    }
+    
+    return (
+        <div className='fix-box' onClick={changeColor}> 
+            <h2>Selection</h2>       
+        </div>
+    )
+}
+
+export default Selection;
