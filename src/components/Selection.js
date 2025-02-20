9c09667d6e43a8b0b3795118088583c3c231e5bd
AYUSH RAWAT <106034287+Ayushrawat777@users.noreply.github.com>
2025-02-20 14:26:07 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
 
 const Selection = ({ item, selectedColor }) => {
   const [background, setBackground] = useState(item.background); // Initial background color
